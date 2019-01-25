ca35edf5f9053a37ed503cf54f20d0709108d122
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-01-25 07:04:33 +0530

    code and test to add an elemnt to the middle in sorted order

--- InsertionSortWithCircularArray.js
+++ InsertionSortWithCircularArray.js
@@ -4,7 +4,7 @@ const insertSmallest = (value, circularArray) => {
     circularArray.head = circularArray.head === 0 ? circularArray.len - 1 : circularArray.head - 1;
   }
   circularArray.array[circularArray.head] = value;
-  circularArray.valid+=1;
+  circularArray.valid += 1;
   return circularArray;
 };
 const insertLargest = (value, circularArray) => {
@@ -13,15 +13,61 @@ const insertLargest = (value, circularArray) => {
     circularArray.tail = circularArray.tail === circularArray.len - 1 ? 0 : circularArray.tail + 1;
   }
   circularArray.array[circularArray.tail] = value;
-  circularArray.valid+=1;
+  circularArray.valid += 1;
   return circularArray;
 };
-const findPosition=(value,circularArray)=>{
-    let countOfLeft=0;
-    while (circularArray.array[circularArray.head] < value && circularArray.array[circularArray.head] !== -1){
-        circularArray.head = circularArray.head === circularArray.len - 1 ? 0 : circularArray.head + 1;
-        countOfLeft+=1;
+const findPosition = (value, circularArray) => {
+  let countOfLeft = 0;
+  let head = circularArray.head;
+  while (circularArray.array[head] < value && circularArray.array[head] !== -1) {
+    head = head === circularArray.len - 1 ? 0 : head + 1;
+    countOfLeft += 1;
+  }
+  return [countOfLeft, circularArray.valid - countOfLeft];
+};
+const insertMiddle = (value, circularArray) => {
+  [leftNo, rightNo] = findPosition(value, circularArray);
+  // console.log([leftNo,rightNo]);
+  if (leftNo < rightNo) {
+    let next = circularArray.head === 0 ? circularArray.len - 1 : circularArray.head - 1;
+    let current = circularArray.head;
+    for (let i = 0; i < leftNo; i++) {
+      // console.log('next',next,'current',current);
+      circularArray.array[next] = circularArray.array[current];
+      next = current;
+      current = next === circularArray.len - 1 ? 0 : next + 1;
+      // console.log(circularArray.array)
     }
-    return [countOfLeft,circularArray.valid-countOfLeft];
-} 
-module.exports = { insertSmallest, insertLargest, findPosition };
+    circularArray.array[next] = value;
+
+    circularArray.valid += 1;
+    circularArray.head = circularArray.head === 0 ? circularArray.len - 1 : circularArray.head - 1;
+  } else {
+    let next = circularArray.tail === circularArray.len - 1 ? 0 : circularArray.tail + 1;
+    let current = circularArray.tail;
+    for (let i = 0; i < rightNo; i++) {
+      // console.log('next',next,'current',current);
+      circularArray.array[next] = circularArray.array[current];
+      next = current;
+      current = next === 0 ? circularArray.len - 1 : next - 1;
+      // console.log(circularArray.array)
+    }
+    circularArray.array[next] = value;
+
+    circularArray.valid += 1;
+    circularArray.tail = circularArray.tail === circularArray.len - 1 ? 0 : circularArray.tail + 1;
+  }
+
+  return circularArray;
+};
+
+/*console.log(insertMiddle(9, {
+    array: [3,4,8,10,45,-1,-1,1 ],
+    tail: 4,
+    head: 7,
+    len: 8,
+    valid:6
+}));*/
+module.exports = {
+  insertSmallest, insertLargest, findPosition, insertMiddle,
+};
--- InsertionSortWithCircularArray.test.js
+++ InsertionSortWithCircularArray.test.js
@@ -7,14 +7,14 @@ describe('insertSmallest', () => {
       tail: 3,
       head: 0,
       len: 7,
-      valid:4
+      valid: 4,
     };
     expect(sortingArray.insertSmallest(2, circularArray)).toEqual({
       array: [3, 4, 5, 6, -1, -1, 2],
       tail: 3,
       head: 6,
       len: 7,
-      valid:5
+      valid: 5,
     });
   });
   it('should insert the given value to the head (modulo len) of the circular array', () => {
@@ -23,14 +23,14 @@ describe('insertSmallest', () => {
       tail: 3,
       head: 0,
       len: 7,
-      valid:5
+      valid: 5,
     };
     expect(sortingArray.insertSmallest(1, circularArray)).toEqual({
       array: [3, 4, 5, 6, -1, 1, 2],
       tail: 3,
       head: 5,
       len: 7,
-      valid:6
+      valid: 6,
     });
   });
   it('should insert the given value to the head (modulo len) of the circular array', () => {
@@ -39,14 +39,14 @@ describe('insertSmallest', () => {
       tail: 0,
       head: 0,
       len: 7,
-      valid:0
+      valid: 0,
     };
     expect(sortingArray.insertSmallest(1, circularArray)).toEqual({
       array: [1, -1, -1, -1, -1, -1, -1],
       tail: 0,
       head: 0,
       len: 7,
-      valid:1
+      valid: 1,
     });
   });
 });
@@ -58,14 +58,14 @@ describe('insertLargest', () => {
       tail: 3,
       head: 0,
       len: 6,
-      valid:4
+      valid: 4,
     };
     expect(sortingArray.insertLargest(7, circularArray)).toEqual({
       array: [1, 2, 3, 4, 7, -1],
       tail: 4,
       head: 0,
       len: 6,
-      valid:5
+      valid: 5,
     });
   });
   it('should insert the given value to the tail (modulo len) of the circular array', () => {
@@ -74,14 +74,14 @@ describe('insertLargest', () => {
       tail: 4,
       head: 6,
       len: 7,
-      valid:6
+      valid: 6,
     };
     expect(sortingArray.insertLargest(8, circularArray)).toEqual({
       array: [3, 4, 5, 6, 7, 8, 2],
       tail: 5,
       head: 6,
       len: 7,
-      valid:7
+      valid: 7,
     });
   });
 
@@ -91,47 +91,115 @@ describe('insertLargest', () => {
       tail: 6,
       head: 1,
       len: 7,
-      valid:6
+      valid: 6,
     };
     expect(sortingArray.insertLargest(11, circularArray)).toEqual({
       array: [11, 4, 5, 6, 7, 8, 10],
       tail: 0,
       head: 1,
       len: 7,
-      valid:7
+      valid: 7,
     });
   });
 });
-  describe('findPosition', () => {
-    it('should return an array which has the number of elemnts smaller than a given value and the number of elements larger than the given element in the circular array', () => {
-      const circularArray = {
-        array: [1, 2, 3, 5, -1, -1],
-        tail: 3,
-        head: 0,
-        len: 6,
-        valid:4
-      };
-      expect(sortingArray.findPosition(4, circularArray)).toEqual([3,1]);
+describe('findPosition', () => {
+  it('should return an array which has the number of elemnts smaller than a given value and the number of elements larger than the given element in the circular array', () => {
+    const circularArray = {
+      array: [1, 2, 3, 5, -1, -1],
+      tail: 3,
+      head: 0,
+      len: 6,
+      valid: 4,
+    };
+    expect(sortingArray.findPosition(4, circularArray)).toEqual([3, 1]);
+  });
+  it('should return an array which has the number of elemnts smaller than a given value and the number of elements larger than the given element in the circular array', () => {
+    const circularArray = {
+      array: [3, 4, 5, 9, 10, -1, 2],
+      tail: 4,
+      head: 6,
+      len: 7,
+      valid: 6,
+    };
+    expect(sortingArray.findPosition(8, circularArray)).toEqual([4, 2]);
+  });
+
+  it('should return an array which has the number of elemnts smaller than a given value and the number of elements larger than the given element in the circular array', () => {
+    const circularArray = {
+      array: [-1, 4, 6, 7, 8, 9, 10],
+      tail: 6,
+      head: 1,
+      len: 7,
+      valid: 6,
+    };
+    expect(sortingArray.findPosition(5, circularArray)).toEqual([1, 5]);
+  });
+});
+
+
+describe('insertMiddle', () => {
+  it('should insert the given value at the right position of the circular array', () => {
+    const circularArray = {
+      array: [2, 4, 6, 9, 10, -1, 1],
+      tail: 4,
+      head: 6,
+      len: 7,
+      valid: 6,
+    };
+    expect(sortingArray.insertMiddle(8, circularArray)).toEqual({
+      array: [2, 4, 6, 8, 9, 10, 1],
+      tail: 5,
+      head: 6,
+      len: 7,
+      valid: 7,
     });
-    it('should return an array which has the number of elemnts smaller than a given value and the number of elements larger than the given element in the circular array', () => {
-      const circularArray = {
-        array: [3, 4, 5, 9, 10, -1, 2],
-        tail: 4,
-        head: 6,
-        len: 7,
-        valid:6
-      };
-      expect(sortingArray.findPosition(8, circularArray)).toEqual([4,2]);
+  });
+  it('should insert the given value at the right position of the circular array', () => {
+    const circularArray = {
+      array: [3, 4, 8, 10, 45, -1, -1, 1],
+      tail: 4,
+      head: 7,
+      len: 8,
+      valid: 6,
+    };
+    expect(sortingArray.insertMiddle(9, circularArray)).toEqual({
+      array: [3, 4, 8, 9, 10, 45, -1, 1],
+      tail: 5,
+      head: 7,
+      len: 8,
+      valid: 7,
     });
-  
-    it('should return an array which has the number of elemnts smaller than a given value and the number of elements larger than the given element in the circular array', () => {
-      const circularArray = {
-        array: [-1, 4, 6, 7, 8, 9, 10],
-        tail: 6,
-        head: 1,
-        len: 7,
-        valid:6
-      };
-      expect(sortingArray.findPosition(5, circularArray)).toEqual([1,5]);
+  });
+  it('should insert the given value at the right position of the circular array', () => {
+    const circularArray = {
+      array: [1, 2, 3, 5, 6, 7, -1],
+      tail: 5,
+      head: 0,
+      len: 7,
+      valid: 6,
+    };
+    expect(sortingArray.insertMiddle(4, circularArray)).toEqual({
+      array: [1, 2, 3, 4, 5, 6, 7],
+      tail: 6,
+      head: 0,
+      len: 7,
+      valid: 7,
     });
+  });
+  it('should insert the given value at the right position of the circular array', () => {
+    const circularArray = {
+      array: [1, 2, 3, 5, 6, 7, -1],
+      tail: 5,
+      head: 0,
+      len: 7,
+      valid: 6,
+    };
+    expect(sortingArray.insertMiddle(4, circularArray)).not.toEqual({
+      array: [2, 3, 4, 5, 6, 7, 1],
+      tail: 5,
+      head: 6,
+      len: 7,
+      valid: 7,
+    });
+  });
 });
